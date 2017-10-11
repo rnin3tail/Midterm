c1689ac97682e14d0df2972ada423a04ea33b731
Roman Chowdhury <rnin3tail@gmail.com>
2017-10-11 11:39:10 -0400

    Player Setup
    Created Player Ship, Wall Boundaries, Player Movement, Player UI,  player shooting, camera functioning.

---
+++ Midterm_Project/Assets/Scripts/BulletController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulletController : MonoBehaviour { 
+
+	public float speed;
+	public GameObject bullet;
+	private PlayerController player;
+
+
+
+	public GameObject enemyShipDeath;
+	public GameObject meteorDeath;
+
+	AudioSource audio;
+	public AudioClip enemyShipSfx;
+	public AudioClip meteorSfx;
+
+
+	void Start () {
+
+// Obtain playercontroller and audio component
+
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+		audio = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<Rigidbody2D> ().AddForce(transform.up * speed);
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		
+//Destroys enemy ships.
+		if (other.tag == "enemyShip") {
+			audio.PlayOneShot (enemyShipSfx, 1.0f);
+			Instantiate (enemyShipDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+
+		}
+//Destroys meteors
+		if (other.tag == "meteor") {
+			audio.PlayOneShot (meteorSfx, 1.0f); 
+			Instantiate (meteorDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+		}
+//Destroys bullet if it hits anything other than an enemy ship or player.
+		if (other.tag != "Player" && other.tag != "Bullet") {
+			if (other.transform.parent != null) {
+				if (other.transform.parent.tag == "Player") {
+					return;
+				}
+			}
+			Debug.Log ("Hit: " + other.tag);
+			Destroy (this.gameObject);
+		}
+
+	}
+}
---
+++ Midterm_Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero; // x, y, and z coordinates
+	public Transform target; // transform helps us change position of objects. Ex. Player
+
+	public bool  bounds;
+
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position); //place the camera's view (viewport) on to the player object.
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(.50f,.50f,point.z)); //character pos - camera position. Create new point for camera.
+			Vector3 destination = transform.position + delta; // changing point of camera 
+
+			transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, dampTime); // makes the camera follow smoothly.
+
+			if (bounds) {
+				transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x), Mathf.Clamp(transform.position.y, minCameraPos.y,maxCameraPos.y), Mathf.Clamp (transform.position.z, minCameraPos.z, maxCameraPos.z));
+				// makes sure that the camera stays within the min and max position set by chosen coordinates
+
+			}
+
+		}
+	}
+}
+
